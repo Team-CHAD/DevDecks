ff0dd3cfa392b7437112a4b658fd01e4fc4678f0
Cheng Ly <chengsieuly@gmail.com>
2017-01-20 10:41:26 -0800

    fix: Issue with ts-node pathing on Windows 10
    Settings for types could be set in tsconfig.json under
    "types": [...]. However, this method only works for Mac.
    On Windows 10, pathing is done differently so it cannot locate
    the type definitions. We must specify absolute paths to these types
    to make it compatible with Windows 10.
    
    https://github.com/TypeStrong/ts-node/issues/168

--- test/setup.ts
+++ test/setup.ts
@@ -1,4 +1,9 @@
-import 'babel-polyfill';
+// Issue with Windows 10 pathing so types could not be set in tsconfig.json
+// so we need to specify absolute paths
+/// <reference path="../node_modules/@types/chai/index.d.ts" />
+/// <reference path="../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
+
 import { jsdom } from 'jsdom';
 
 declare var global: any;
